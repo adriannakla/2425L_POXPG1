3ad6b40ce22bd501dcdc2d735d59dcf1159d0623
adriannakla <s30544@pjwstk.edu.pl>
2025-05-12 13:44:04 +0200

    Prawie koniec gry

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,9 +6,10 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public string Word = "after";
+    public int LivesCount = 10;
     public TextMeshProUGUI MainWord;
     public TextMeshProUGUI OurInput;
-    public TextMeshProUGUI SubmittedInput;
+    public TextMeshProUGUI SuggestionWord;
     public TMP_InputField InputField;
 
 
@@ -16,6 +17,7 @@ public class GameController : MonoBehaviour
     void Start()
     {
         MainWord.text = Word;
+        OurInput.text = $"Zostalo prob: {LivesCount}";
     }
 
     public void OnInputChanged(string input)
@@ -26,7 +28,58 @@ public class GameController : MonoBehaviour
 
     public void OnClick()
     {
-        SubmittedInput.text = OurInput.text;
+        Debug.Log($"Przycisk zostal klikniety.");
+        LivesCount = LivesCount - 1;
+        OurInput.text = $"Zostalo prob: {LivesCount}";
+               
+        if(Word == InputField.text)
+        {
+            Debug.Log($"Prawidlowe slowo");
+            return;
+        }
+
+        if (Word.Length != InputField.text.Length)
+        {
+            SuggestionWord.text = $"Dlugosc sie nie zgadza.";
+            return;
+        }
+
+        int bullsCount = CountBulls();
+        int cowsCount = CountCows();
+
+        SuggestionWord.text = $"Bulls: {bullsCount} and Cows: {cowsCount}";
+    }
+
+    public int CountBulls()
+    {
+        int result = 0;
+
+        for (int i = 0; i < Word.Length; i++)
+        {
+            if (Word[i] == InputField.text[i])
+            {
+                result++;
+            }
+        }
+
+        return result;
+    }
+
+    public int CountCows()
+    {
+        int result = 0;
+        for (int i = 0; i < Word.Length; i++)
+        {
+            // || => or
+            // && => and
+            if (Word[i] != InputField.text[i] && Word.Contains(InputField.text[i]))
+            {
+                result++;
+            }
+        }
+
+
+        return result;
     }
 
     // Update is called once per frame
